4b8ec5559ace51627de6cf23646fb7a69cc1b01d
Mirza <mohammedmirsa@gmail.com>
2024-04-28 16:45:06 +0530

    navigation to chemicals page from banner 0 completed

--- lib/view/screens/home/widgets/slider.dart
+++ lib/view/screens/home/widgets/slider.dart
@@ -1,13 +1,25 @@
+import 'dart:developer';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shoppe_customer/controller/myController/banner_controller.dart';
+import 'package:shoppe_customer/controller/myController/categoryController.dart';
 import 'package:shoppe_customer/helper/responsive_helper.dart';
 import 'package:shoppe_customer/view/base/custom_image.dart';
+import 'package:shoppe_customer/view/screens/carshoppe/categorywise_productlisting.dart';
 
 class ImageSlider extends StatelessWidget {
   const ImageSlider({super.key});
   // var bannerController = Get.find<BannerController>();
+
+  void getCategoryData() async {
+    await Get.find<ProductCategoryController>().fetchCategoryData();
+    await Get.find<BannerController>().bannerData(
+      'shoppeCategoriesListing',
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // bannerController.bannerData('Newbanners');
@@ -38,35 +50,63 @@ class ImageSlider extends StatelessWidget {
                   disableCenter: true,
                   autoPlayInterval: const Duration(seconds: 6),
                   onPageChanged: (index, reason) {},
-
                 ),
                 itemCount: bannerController.banner!.length,
                 itemBuilder: (context, index, _) {
-                  return InkWell(
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 8,right: 8),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.black.withOpacity(0.5),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.grey[Get.isDarkMode ? 800 : 200]!,
-                                  spreadRadius: 1,
-                                  blurRadius: 5)
-                            ],
-                          ),
-                          clipBehavior: Clip.hardEdge,
-                          child: ClipRRect(
-                            child: CustomImage(
-                              image: bannerController.banner![index],
-                              height: 150,
-                              fit: BoxFit.fill,
+                  return GetBuilder<ProductCategoryController>(
+                      builder: (categoryModelController) {
+                    return InkWell(
+                        onTap: () {
+                          getCategoryData();
+                          log(bannerController.banner![index]);
+                          log(index.toString());
+
+                          if (index == 0) {
+                            var productLocations =
+                                Get.find<ProductCategoryController>()
+                                    .productLocationList;
+                            var categoryId =
+                                categoryModelController.categoryList[2].id;
+
+                            categoryModelController
+                                .fetchSubCategoryData(categoryId);
+                            categoryModelController.fetchProductsByCategory(
+                                categoryId, productLocations, '1');
+                            Get.to(() => CategoryWiseProductListing(
+                                  subCategories:
+                                      categoryModelController.subCategoryList,
+                                  data: categoryModelController.categoryList[0],
+                                  title: categoryModelController
+                                      .categoryList[2].name,
+                                  idx: 0,
+                                ));
+                          } else if (index == 1) {}
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8, right: 8),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.black.withOpacity(0.5),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors
+                                        .grey[Get.isDarkMode ? 800 : 200]!,
+                                    spreadRadius: 1,
+                                    blurRadius: 5)
+                              ],
+                            ),
+                            clipBehavior: Clip.hardEdge,
+                            child: ClipRRect(
+                              child: CustomImage(
+                                image: bannerController.banner![index],
+                                height: 150,
+                                fit: BoxFit.fill,
+                              ),
                             ),
                           ),
-                        ),
-                      ));
+                        ));
+                  });
                 },
               ))
           : SizedBox(
