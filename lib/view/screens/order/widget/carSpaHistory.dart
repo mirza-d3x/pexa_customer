81fc26efae6ab67144d0881f88922ec617b439c4
Mirza <mohammedmirsa@gmail.com>
2024-05-02 23:32:49 +0530

    bug fix in order history

--- lib/view/screens/order/widget/carSpaHistory.dart
+++ lib/view/screens/order/widget/carSpaHistory.dart
@@ -40,68 +40,59 @@ class CarSpaHistoryOrders extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // WidgetsBinding.instance.addPostFrameCallback((_) {
-    //   Get.find<CarSpaController>().getCarSpaHistoryOrders('1');
-    // });
-    return GetBuilder<OrderController>(builder: (orderController) {
-      // carSpaController.getCarSpaHistoryOrders('1');
-      return SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: orderController.OrderListTempHistory.isEmpty
-            ? orderController.isOrderHistoryLoader.isLoading
-                ? Center(
-                    child: LoadingAnimationWidget.twistingDots(
-                      leftDotColor: const Color(0xFF4B4B4D),
-                      rightDotColor: const Color(0xFFf7d417),
-                      size: 50,
-                    ),
-                    // child: DefaultTextStyle(
-                    //   style: smallFontW600(Colors.red),
-                    //   child: AnimatedTextKit(
-                    //     animatedTexts: [
-                    //       WavyAnimatedText('Orders Loading...!'),
-                    //     ],
-                    //     repeatForever: true,
-                    //   ),
-                    // ),
-                  )
-                : const ShopErrorScreen(
-                    title: 'No Order found',
-                    subTitle: 'Look like you haven\'nt made your order yet',
-                    imagePath: Images.empty_order_icon,
-                    route: RouteHelper.carSpaService,
-                  )
-            : SmartRefresher(
-                controller: _refreshController,
-                enablePullDown: true,
-                enablePullUp: true,
-                onRefresh: _onRefresh,
-                onLoading: (orderController.OrderListHistory.length >= 20)
-                    ? _onLoading
-                    : () async {
-                        await Future.delayed(const Duration(milliseconds: 500), () {
-                          _refreshController.loadComplete();
-                        });
-                      },
-                footer: CustomFooter(
-                    builder: (BuildContext context, LoadStatus? mode) {
-                  Widget body;
-                  if (mode == LoadStatus.loading) {
-                    body = const CupertinoActivityIndicator();
-                  } else if (mode == LoadStatus.failed) {
-                    body = const Text("Load Failed!Click retry!");
-                  } else if (mode == LoadStatus.canLoading) {
-                    body = const Text("release to load more");
-                  } else {
-                    body = const Text("No more Data");
-                  }
-                  return SizedBox(
-                    height: 55.0,
-                    child: Center(child: body),
-                  );
-                }),
-                child: ListView.builder(
-                    itemCount: 7,
+    return GetBuilder<OrderController>(
+      builder: (orderController) {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: orderController.OrderListTempHistory.isEmpty
+              ? orderController.isOrderHistoryLoader.isLoading
+                  ? Center(
+                      child: LoadingAnimationWidget.twistingDots(
+                        leftDotColor: const Color(0xFF4B4B4D),
+                        rightDotColor: const Color(0xFFf7d417),
+                        size: 50,
+                      ),
+                    )
+                  : const ShopErrorScreen(
+                      title: 'No Order found',
+                      subTitle: 'Look like you haven\'nt made your order yet',
+                      imagePath: Images.empty_order_icon,
+                      route: RouteHelper.carSpaService,
+                    )
+              : SmartRefresher(
+                  controller: _refreshController,
+                  enablePullDown: true,
+                  enablePullUp: true,
+                  onRefresh: _onRefresh,
+                  onLoading: (orderController.OrderListHistory.length >= 20)
+                      ? _onLoading
+                      : () async {
+                          await Future.delayed(
+                            const Duration(milliseconds: 500),
+                            () {
+                              _refreshController.loadComplete();
+                            },
+                          );
+                        },
+                  footer: CustomFooter(
+                      builder: (BuildContext context, LoadStatus? mode) {
+                    Widget body;
+                    if (mode == LoadStatus.loading) {
+                      body = const CupertinoActivityIndicator();
+                    } else if (mode == LoadStatus.failed) {
+                      body = const Text("Load Failed!Click retry!");
+                    } else if (mode == LoadStatus.canLoading) {
+                      body = const Text("release to load more");
+                    } else {
+                      body = const Text("No more Data");
+                    }
+                    return SizedBox(
+                      height: 55.0,
+                      child: Center(child: body),
+                    );
+                  }),
+                  child: ListView.builder(
+                    itemCount: orderController.OrderListTempHistory.length,
                     /*orderController.OrderListTempHistory.length,*/
                     itemBuilder: (context, index) {
                       return ServiceOrderViewTile(
@@ -109,9 +100,11 @@ class CarSpaHistoryOrders extends StatelessWidget {
                             orderController.OrderListTempHistory[index],
                         mainServiceCategory: MainCategory.CARSPA,
                       );
-                    }),
-              ),
-      );
-    });
+                    },
+                  ),
+                ),
+        );
+      },
+    );
   }
 }
